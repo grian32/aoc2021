83dc81545d8b78369af73920c1cc89ba41fdcf37
Grian <grianguy32@gmail.com>
2021-12-11 15:38:15 +0200

    day 11 not fuck!

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,56 +1,100 @@
+import util.filterNotEmpty
 import java.io.File
-import util.Day11.parseIntoGrid
 
-//private val input = File("src/main/resources/day11.txt").readText()
-private val input = """5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526"""
-
-fun main() {
-    println("PART 1\n---")
-    println(part1())
-    println("PART 2\n---")
-    println(part2())
-}
-
-private fun part1(): Int {
+class Day11 {
+    var grid = File("src/main/resources/day11.txt").readText().parseGrid()
     var flashes = 0
-    val grid = parseIntoGrid(input)
-    for (i in 0..100) {
-        for ((x, i) in grid.rows.withIndex()) {
-            for ((y, j) in i.withIndex()) {
-                grid[x, y] += 1
+
+    fun part1(): Int {
+        for (c in 0 until 100) {
+            var stepFlashes = 0
+            for (x in 0 until 10) {
+                for (y in 0 until 10) {
+                    addAndFlash(x, y)
+                }
+            }
+
+            for (x in 0 until 10) {
+                for (y in 0 until 10) {
+                    if (grid[x][y] == 10) {
+                        stepFlashes++
+                        grid[x][y] = 0
+                    }
+                }
+            }
+
+            flashes += stepFlashes
+        }
+
+
+        return flashes
+    }
+
+    fun part2(): Int {
+        grid = File("src/main/resources/day11.txt").readText().parseGrid()
+
+        for (c in 0 until Int.MAX_VALUE) {
+            var stepFlashes = 0
+            for (x in 0 until 10) {
+                for (y in 0 until 10) {
+                    addAndFlash(x, y)
+                }
+            }
+
+            for (x in 0 until 10) {
+                for (y in 0 until 10) {
+                    if (grid[x][y] == 10) {
+                        stepFlashes++
+                        grid[x][y] = 0
+                    }
+                }
+            }
+
+            if (grid.flatten().count { it == 0 } == 100) {
+                return c + 1 // off by one dunno why
             }
         }
 
-        for ((x, i) in grid.rows.withIndex()) {
-            for ((y, j) in i.withIndex()) {
-                if (grid[x, y] >= 9) {
-                   flashes += grid.flash(x, y)
-                }
-            }
-        }
+        return 0
+    }
 
-        for ((x, i) in grid.rows.withIndex()) {
-            for ((y, j) in i.withIndex()) {
-                if (grid[x, y] >= 9) {
-                    grid[x, y] = 0
-                }
+    private fun addAndFlash(x: Int, y: Int) {
+        if (x < 0 || x >= 10 || y < 0 || y >= 10) return
+
+        val value = grid[x][y]
+
+        if (value < 9) {
+            grid[x][y] += 1
+        } else if (value == 9) {
+            grid[x][y] = 10
+            getAdjacentPoints(x, y).forEach {
+                addAndFlash(it.first, it.second)
             }
         }
     }
 
-    return flashes
-}
+    private fun getAdjacentPoints(x: Int, y: Int): List<Pair<Int, Int>> {
+        val points: MutableList<Pair<Int, Int>> = mutableListOf()
 
-private fun part2(): Int {
+        for (x0 in -1..1) {
+            for (y0 in -1..1) {
+                points.add(x + x0 to y + y0)
+            }
+        }
 
-    return 0
+        return points
+    }
+
+    private fun String.parseGrid(): MutableList<MutableList<Int>> {
+        return split("\n").map { it.split("").filterNotEmpty().map(String::toInt).toMutableList() }.toMutableList()
+    }
+ }
+
+fun main() {
+    val day11 = Day11()
+
+    println("PART 1\n---")
+    println(day11.part1())
+    println("PART 2\n---")
+    println(day11.part2())
 }
